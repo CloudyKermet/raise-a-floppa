2434216d6dd365c65a58d9219fdb31d328b03c16
CloudyKermet <gamingbright2011@gmail.com>
2026-06-25 17:31:08 -0500

    Implement autorent functionality
    Added autorent feature to automatically request rent and raise for roommates.

--- source.lua
+++ source.lua
@@ -5,6 +5,8 @@ local humanoidRootPart = character:WaitForChild("HumanoidRootPart")
 local RunService = game:GetService("RunService")
 local autocollect = false
 local autoclick = false
+local autorent = false
+
 
 local Window = Rayfield:CreateWindow({
    Name = "raise a floopa",
@@ -25,6 +27,7 @@ RunService.RenderStepped:Connect(function(dt)
 if autocollect then
 local money = workspace:FindFirstChild("Money")
 local moneybag = workspace:FindFirstChild("Money Bag")
+local rentbag = workspace:FindFirstChild("Rent")
          
          if money then
         firetouchinterest(humanoidRootPart, money, 0)
@@ -33,10 +36,23 @@ local moneybag = workspace:FindFirstChild("Money Bag")
          if moneybag then
          firetouchinterest(humanoidRootPart, moneybag, 0)
          end
+
+         if rentbag then
+         firetouchinterest(humanoidRootPart, rentbag, 0)
+         end
 end
       if autoclick then
          fireclickdetector(workspace.Floppa.ClickDetector)
 end
+
+      if autorent then
+      if workspace.Roommate then
+      if workspace.Roommate.Head.Rent.Value == true and workspace.Roommate.Head.CanRaise.Value == true then
+      game:GetService("ReplicatedStorage").RequestRent:FireServer()
+      game:GetService("ReplicatedStorage").RequestRaise:FireServer()
+            end
+         end
+      end
 end)
 
 local Tab1 = Window:CreateTab("Auto", 4483362458)
@@ -56,5 +72,14 @@ local tgl2 = Tab1:CreateToggle({
    Flag = "Toggle1",
    Callback = function(Value)
   autoclick = not autoclick
+   end,
+})
+
+local tgl3 = Tab1:CreateToggle({
+   Name = "Auto-Rent",
+   CurrentValue = false,
+   Flag = "Toggle1",
+   Callback = function(Value)
+  autorent = not autorent
    end,
 })
